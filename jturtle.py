217d83038ca6719fd29cb741d3dd5f95de1d1d76
kazukikby <39661778+kazukikby@users.noreply.github.com>
2022-02-27 12:42:10 +0900

    Add files via upload

--- jturtle.py
+++ jturtle.py
@@ -1,19 +1,20 @@
 # jupyter上でタートルグラフィクスもどきを
 # インラインで表示する
 import math
-import time
 from PIL import Image, ImageDraw
-from IPython.display import display, clear_output
+from IPython.display import HTML, display, clear_output
+import io
+import base64
+import random
 
 class JTurtle:
     def __init__(self):
         self.reset()
-        self.show()
 
     def reset(self):
         self.wh = (800, 600)        
         self.bg_fill = (240, 240, 240)
-        self.im = Image.new("RGB", self.wh, self.bg_fill)
+        self.im = Image.new('RGB', self.wh, self.bg_fill)
         self.draw = ImageDraw.Draw(self.im)
         self.angle = 0
         self.x = self.wh[0]/2
@@ -21,11 +22,23 @@ class JTurtle:
         self.is_pen_up = False        
         self.fill = (0, 0, 0)
         self.speed = 2 
-        
-    def show(self):
+        self.images = []
+        self.idx = 0
+        # self.images.append(self.im.copy())
+        self.append_dummy_image()
         clear_output(wait=True)
-        display(self.im)
-        time.sleep(0.01)
+    
+    def show_progress(self):
+        print("making frames:", self.idx)
+        self.idx += 1
+        clear_output(wait=True)
+    
+    def show_msg(self, msg):
+        print(msg)
+        clear_output(wait=True)
+
+    def append_image(self):
+        self.images.append(self.im.copy())
         
     def fd(self, r):
         for i in range(round(r/self.speed)):
@@ -34,7 +47,8 @@ class JTurtle:
 
             if self.is_pen_up == False:
                 self.draw.line((self.x, self.y, nx, ny), fill=self.fill, width=2)
-                self.show()
+                self.append_image()
+                self.show_progress()
 
             self.x = nx
             self.y = ny
@@ -42,12 +56,14 @@ class JTurtle:
     def circle(self, r):
         if self.is_pen_up == False:
             self.draw.ellipse((self.x-r, self.y-(2*r), self.x+r, self.y), fill=None, outline=self.fill, width=2)
-            self.show()
+            self.append_image()
+            self.show_progress()
 
     def ellipse(self, w, h):
         if self.is_pen_up == False:
             self.draw.ellipse((self.x-w/2, self.y-h, self.x+w/2, self.y), fill=None, outline=self.fill, width=2)
-            self.show()
+            self.append_image()
+            self.show_progress()
 
     def pu(self):
         self.is_pen_up = True
@@ -64,7 +80,8 @@ class JTurtle:
     def goto(self, x, y):
         if self.is_pen_up == False:
             self.draw.line((self.x, self.y, x, y), fill=self.fill, width=2)
-            self.show()
+            self.append_image()
+            self.show_progress()
         self.x = x
         self.y = y
                 
@@ -73,8 +90,21 @@ class JTurtle:
         
     def color(self, r, g, b):
         self.fill = (r, g, b)
-        
+    
+    def append_dummy_image(self):
+        # 画像キャッシュ対策(最初のフレームに目立たない色で点を追加)
+        rx = random.randrange(self.wh[0])
+        ry = random.randrange(self.wh[1])
+        self.draw.line((rx, ry, rx, ry), fill=(240, 240, 239), width=1)
+        self.append_image()
+
     def done(self):
+        buf = io.BytesIO()
+        self.images[0].save(buf, format="PNG", save_all=True, append_images=self.images[1:], optimize=False, duration=20, loop=2) 
+        buf.seek(0)
+        img_str = f"""<img src="data:image/png;base64,{base64.b64encode(buf.getvalue()).decode('ascii')}" />"""
+        buf.close()
+        display(HTML(img_str))
         self.reset()
 
 _jturtle = JTurtle()
